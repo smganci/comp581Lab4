cf85afefcd191c1a5132bfb9b778f4072dc299cd
Sarah Ganci <sarahganci@gmail.com>
2018-11-26 21:12:12 -0500

    charlie_work currently works on simple course--may need to adjust rotation angle, proximity range to goal, remove print statements.  also need to test in sharp angle mode

--- Lab4.java
+++ Lab4.java
@@ -3,7 +3,7 @@ package lab4;
 public class Lab4 {
 
 	public static void main(String[] args) {
-		Charlie1 charlie = new Charlie1();
+		Charlie_Work charlie = new Charlie_Work();
 		System.out.println("Press button to start");
 //		charlie.rotateSonic(15);
 		charlie.buttonWait();
